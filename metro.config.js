9c122d855b2aa2268c3931c5a22621d6de4872b0
ahmedbna <hi@ahmedbna.com>
2025-11-30 03:47:38 +0200

    clean

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-// Add support for .bin and .wav assets
-config.resolver.assetExts.push("bin", "wav");
-
-module.exports = config;
